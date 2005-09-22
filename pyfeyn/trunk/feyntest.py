b12301de5936498eca3240a939397c84f5b0f3e5
buckley <buckley@07e9fb06-9504-0410-9509-f90c9c1de517>
2005-09-22 15:51:53 +0000

    Using the built-in patterns
    git-svn-id: http://svn.hepforge.org/pyfeyn@1705 07e9fb06-9504-0410-9509-f90c9c1de517

--- pyfeyn/trunk/feyntest.py
+++ pyfeyn/trunk/feyntest.py
@@ -6,19 +6,18 @@ from pyx import *
 c = canvas.canvas()
 fd = FeynDiagram()
 
-p1 = FilledPoint(1, -1, 0.15)
+p1 = FilledPoint(2, -2, 0.3)
 p1.fillstyle([color.rgb.blue])
-p2 = Point(-1, 1, 0.15)
+p2 = FilledPoint(-2, 2, 0.3)
+p2.fillstyle([pattern.hatched135])
 line = Line(p1, p2)
 line.style([deco.barrow.normal, deco.earrow.normal])
-line.arcThru(Point(0, 0))
+line.arcThru(Point(3, 0))
 
 fd.add(line)
 fd.add(p1)
 fd.add(p2)
 
 fd.draw(c)
-
-#c.stroke(path.line(0, 1, 2, 2), [deco.barrow.normal, deco.earrow.normal])
 
 c.writeEPSfile("test")
