fdfc44985329537b1a3db40bf918d6d1c5e433f9
buckley <buckley@07e9fb06-9504-0410-9509-f90c9c1de517>
2005-10-07 16:00:12 +0000

    Starting the coil deformer
    git-svn-id: http://svn.hepforge.org/pyfeyn@1736 07e9fb06-9504-0410-9509-f90c9c1de517

--- pyfeyn/trunk/feyndiagram.py
+++ pyfeyn/trunk/feyndiagram.py
@@ -253,7 +253,7 @@ class Gluon(DecoratedLine):
         
 
 class Arrow(deco.deco, attr.attr):
-    "Arrow for Feynman diagram lines"
+    """Arrow for Feynman diagram lines"""
     def __init__(self, pos=0.5, size=6*unit.v_pt, angle=45, constriction=0.8):
         self.pos = pos
         self.size = size
@@ -268,4 +268,144 @@ class Arrow(deco.deco, attr.attr):
         arrowpath = deco._arrowhead(arrowtopath, self.size, 45, constrictionlen, 1)
         dp.ornaments.fill(arrowpath)
         return dp
-    
+
+
+
+
+class Coil(deformer.deformer):
+    """Fancy version of the PyX default cycloid deformer, with an oscillating
+    x-velocity and a 3D-look option.
+    """
+
+    def __init__(self, radius=0.5*unit.t_cm, halfloops=10,
+                 skipfirst=1*unit.t_cm, skiplast=1*unit.t_cm,
+                 curvesperhloop=3, sign=1, turnangle=45):
+        self.skipfirst = skipfirst
+        self.skiplast = skiplast
+        self.radius = radius
+        self.halfloops = halfloops
+        self.curvesperhloop = curvesperhloop
+        self.sign = sign
+        self.turnangle = turnangle
+
+    def __call__(self, radius=None, halfloops=None,
+                 skipfirst=None, skiplast=None,
+                 curvesperhloop=None, sign=None, turnangle=None):
+        if radius is None:
+            radius = self.radius
+        if halfloops is None:
+            halfloops = self.halfloops
+        if skipfirst is None:
+            skipfirst = self.skipfirst
+        if skiplast is None:
+            skiplast = self.skiplast
+        if curvesperhloop is None:
+            curvesperhloop = self.curvesperhloop
+        if sign is None:
+            sign = self.sign
+        if turnangle is None:
+            turnangle = self.turnangle
+        return gluon(radius=radius, halfloops=halfloops,
+                     skipfirst=skipfirst, skiplast=skiplast,
+                     curvesperhloop=curvesperhloop,
+                     sign=sign, turnangle=turnangle)
+
+    def deform(self, abasepath):
+        basepath = abasepath.normpath()
+        for sp in basepath.normsubpaths:
+            if sp == basepath.normsubpaths[0]:
+                cycloidpath = self.deformsubpath(sp)
+            else:
+                cycloidpath.join(self.deformsubpath(sp))
+        return cycloidpath
+
+
+    def deformsubpath(self, subpath):
+        skipfirst = abs(unit.topt(self.skipfirst))
+        skiplast = abs(unit.topt(self.skiplast))
+        radius = abs(unit.topt(self.radius))
+        turnangle = self.turnangle * math.pi / 180.0
+
+        cosTurn = math.cos(turnangle)
+        sinTurn = math.sin(turnangle)
+
+        # make list of the lengths and parameters at points on subpath where we will add cycloid-points
+        totlength = subpath.arclen_pt()
+        if totlength <= skipfirst + skiplast + 2*radius*sinTurn:
+            sys.stderr.write("*** PyX Warning: subpath is too short for deformation with cycloid -- skipping...\n")
+            return path.normpath([subpath])
+
+        # parametrisation is in rotation-angle around the basepath
+        # differences in length, angle ... between two basepoints
+        # and between basepoints and controlpoints
+        Dphi = math.pi / self.curvesperhloop
+        phis = [i * Dphi for i in range(self.halfloops * self.curvesperhloop + 1)]
+        DzDphi = (totlength - skipfirst - skiplast - 2*radius*sinTurn) * 1.0 / (self.halfloops * math.pi * cosTurn)
+        Dz = (totlength - skipfirst - skiplast - 2*radius*sinTurn) * 1.0 / (self.halfloops * self.curvesperhloop * cosTurn)
+        zs = [i * Dz for i in range(self.halfloops * self.curvesperhloop + 1)]
+        # from path._arctobcurve:
+        # optimal relative distance along tangent for second and third control point
+        L = 4 * radius * (1 - math.cos(Dphi/2)) / (3 * math.sin(Dphi/2))
+
+        # Now the transformation of z into the turned coordinate system
+        Zs = [ skipfirst + radius*sinTurn # here the coordinate z starts
+             - sinTurn*radius*math.cos(phi) + cosTurn*DzDphi*phi # the transformed z-coordinate
+             for phi in phis]
+        params = subpath._arclentoparam_pt(Zs)[0]
+
+        # get the positions of the splitpoints in the cycloid
+        points = []
+        for phi, param in zip(phis, params):
+            # the cycloid is a circle that is stretched along the subpath
+            # here are the points of that circle
+            basetrafo = subpath.trafo(param)
+
+            # The point on the cycloid, in the basepath's local coordinate system
+            baseZ, baseY = 0, radius*math.sin(phi)
+
+            # The tangent there, also in local coords
+            tangentX = -cosTurn*radius*math.sin(phi) + sinTurn*DzDphi
+            tangentY = radius*math.cos(phi)
+            tangentZ = sinTurn*radius*math.sin(phi) + DzDphi*cosTurn
+            norm = math.sqrt(tangentX*tangentX + tangentY*tangentY + tangentZ*tangentZ)
+            tangentY, tangentZ = tangentY/norm, tangentZ/norm
+
+            # Respect the curvature of the basepath for the cycloid's curvature
+            # XXX this is only a heuristic, not a "true" expression for
+            #     the curvature in curved coordinate systems
+            pathradius = subpath.curvradius_pt(param)
+            if pathradius is not None:
+                factor = (pathradius - baseY) / pathradius
+                factor = abs(factor)
+            else:
+                factor = 1
+            l = L * factor
+
+            # The control points prior and after the point on the cycloid
+            preeZ, preeY = baseZ - l * tangentZ, baseY - l * tangentY
+            postZ, postY = baseZ + l * tangentZ, baseY + l * tangentY
+
+            # Now put everything at the proper place
+            points.append(basetrafo._apply(preeZ, self.sign * preeY) +
+                          basetrafo._apply(baseZ, self.sign * baseY) +
+                          basetrafo._apply(postZ, self.sign * postY))
+
+        if len(points) <= 1:
+            sys.stderr.write("*** PyX Warning: subpath is too short for deformation with cycloid -- skipping...\n")
+            return path.normpath([subpath])
+
+        # Build the path from the pointlist
+        # containing (control x 2,  base x 2, control x 2)
+        if skipfirst > subpath.epsilon:
+            newpath = subpath.split([params[0]])[0]
+            newpath.append(path.normcurve(*(points[0][2:6] + points[1][0:4])))
+            cycloidpath = path.normpath([newpath])
+        else:
+            cycloidpath = path.normpath([path.normsubpath([path.normcurve(*(points[0][2:6] + points[1][0:4]))], 0)])
+        for i in range(1, len(points)-1):
+            cycloidpath.normsubpaths[-1].append(path.normcurve(*(points[i][2:6] + points[i+1][0:4])))
+        if skiplast > subpath.epsilon:
+            cycloidpath.join(path.normpath([subpath.split([params[-1]])[-1]]))
+
+        # That's it
+        return cycloidpath
