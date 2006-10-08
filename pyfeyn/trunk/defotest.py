d82adf5ef6f3244e00d67686f01076de95cdb3c7
buckley <buckley@07e9fb06-9504-0410-9509-f90c9c1de517>
2006-10-08 22:25:57 +0000

    Got a demo of the sine deformer working. Slow and having problems with gradient normalisation, but it's the right idea at least.
    git-svn-id: http://svn.hepforge.org/pyfeyn@1838 07e9fb06-9504-0410-9509-f90c9c1de517

--- pyfeyn/trunk/defotest.py
+++ pyfeyn/trunk/defotest.py
@@ -1,10 +1,50 @@
 from pyx import *
+from math import sin, cos
 
 c = canvas.canvas()
-mypath = path.curve(0.0, 0.0, 1.0, 3.0, 3.0, 3.0, 4.0, 0.0)
-print mypath.arclen()
-mypathsegs = mypath.split([mypath.arclen()/10.0 for i in range(1,11)])
-for seg in mypathsegs[::2]:
-    print seg
-    c.stroke(seg)
+mypath = path.curve(0.0, 0.0, 10.0, 30.0, 30.0, 30.0, 40.0, 0.0)
+numsegs = 100
+omega  = 100.0
+mag = 2.5
+
+lenperseg = mypath.arclen()/float(numsegs)
+lengths = [i*lenperseg for i in range(0,numsegs+1)]
+
+#mypathsegs = mypath.split(lengths[1:-1])
+#for seg in mypathsegs[::2]:
+#    c.stroke(seg)
+
+prevpos = mypath.atbegin()
+prevgrad = (0,0) # fix!
+for l in lengths:
+    tr = mypath.trafo(l)
+    rot = mypath.rotation(l)
+    start = prevpos
+    end = tr.apply(0.0, mag * sin(omega * l.t))
+    m1 = [prevpos[i] + prevgrad[i] for i in [0,1]]
+    grad = rot.apply(0.01, 0.01 * omega * mag * cos(omega * l.t))
+    #print "pos =", end[0], end[1]
+    #print "grad =", grad[0], grad[1]
+    m2 = [end[i] - grad[i] for i in [0,1]]
+    ###
+    s1 = start + end
+    line = path.line(*s1)
+    c.stroke(line)
+    ###
+    s2 = (start[0], start[1], m1[0], m1[1], m2[0], m2[1], end[0], end[1])
+    curve = path.curve(*s2)
+    c1 = path.circle(m1[0], m1[1], 0.2)
+    c2 = path.circle(m2[0], m2[1], 0.2)
+    c.fill(c1, [color.rgb.red])
+    c.stroke(c1)
+    c.stroke(c2)
+    l1 = path.line(start[0], start[1], m1[0], m1[1])
+    l2 = path.line(end[0], end[1], m2[0], m2[1])
+    c.stroke(l1)
+    c.stroke(l2)
+    #c.stroke(curve)
+    ###
+    prevpos = end
+    prevgrad = grad
+
 c.writePDFfile("defotest")
