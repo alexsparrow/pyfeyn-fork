bb1eae0b5d5ebf5b158530a6c73742c56015c728
buckley <buckley@07e9fb06-9504-0410-9509-f90c9c1de517>
2006-12-23 19:19:37 +0000

    Added distutils install script
    git-svn-id: http://svn.hepforge.org/pyfeyn@1862 07e9fb06-9504-0410-9509-f90c9c1de517

---
+++ pyfeyn/trunk/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+setup(name='pyfeyn',
+      version='0.9beta',
+      packages=['pyfeyn'],
+      provides='pyfeyn',
+      requires='pyx >= 0.9',
+      author='Andy Buckley',
+      author_email='andy@insectnation.org',
+      url='http://projects.hepforge.org/pyfeyn/',
+      description='An easy-to-use Python library to help high-energy physicists draw Feynman diagrams.',
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Console',
+                   'Intended Audience :: Science/Research',
+                   'License :: OSI Approved :: GNU General Public License (GPL)',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Artistic Software',
+                   'Topic :: Scientific/Engineering :: Physics']
+      )
