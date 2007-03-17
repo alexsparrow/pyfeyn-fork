65aae392984dccea913b2308f049712db035d6bd
buckley <buckley@07e9fb06-9504-0410-9509-f90c9c1de517>
2007-03-17 07:17:29 +0000

    Using setuptools
    git-svn-id: http://svn.hepforge.org/pyfeyn@1875 07e9fb06-9504-0410-9509-f90c9c1de517

--- pyfeyn/trunk/setup.py
+++ pyfeyn/trunk/setup.py
@@ -1,17 +1,19 @@
 #! /usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='pyfeyn',
-      version='0.2.0b1',
-      packages=['pyfeyn'],
-      provides='pyfeyn',
-      requires='pyx >= 0.9',
-      author='Andy Buckley',
-      author_email='andy@insectnation.org',
-      url='http://projects.hepforge.org/pyfeyn/',
-      description='An easy-to-use Python library to help high-energy physicists draw Feynman diagrams.',
-      classifiers=['Development Status :: 4 - Beta',
+setup(name = 'pyfeyn',
+      version = '0.2.0b1',
+      packages = ['pyfeyn'],
+      provides = 'pyfeyn',
+      requires = 'pyx >= 0.9',
+      author = 'Andy Buckley',
+      author_email = 'andy@insectnation.org',
+      url = 'http://projects.hepforge.org/pyfeyn/',
+      description = 'An easy-to-use Python library to help high-energy physicists draw Feynman diagrams.',
+      keywords = 'feynman hep physics particle diagram',
+      license = 'GPL',
+      classifiers = ['Development Status :: 4 - Beta',
                    'Environment :: Console',
                    'Intended Audience :: Science/Research',
                    'License :: OSI Approved :: GNU General Public License (GPL)',
