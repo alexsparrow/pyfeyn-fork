6308bf669d2aff43aaeb37520c58d9b70e8c2aa5
vonhippg <vonhippg@07e9fb06-9504-0410-9509-f90c9c1de517>
2007-05-01 15:30:13 +0000

    Added FeynML writer example application.
    git-svn-id: http://svn.hepforge.org/pyfeyn@1900 07e9fb06-9504-0410-9509-f90c9c1de517

---
+++ pyfeyn/trunk/examples/feynml-test1.py
@@ -0,0 +1,24 @@
+from pyfeyn import *
+from pyfeyn.feynml import *
+
+fd = FeynDiagram()
+
+in1 = Point(-4,  2)
+in2 = Point(-4, -2)
+in_vtx = Vertex(-2, 0)
+out1 = Point(4, -2)
+out2 = Point(4,  2)
+out_vtx = Vertex(2, 0)
+
+l1 = Label("Drell-Yan QCD vertex correction", x=0, y=2)
+fa1 = Fermion(in1, in_vtx).addArrow().addLabel(r"\Pelectron")
+fa2 = Fermion(in_vtx, in2).addArrow().addLabel(r"\Ppositron")
+bos = Photon(in_vtx, out_vtx).addLabel(r"\Pphoton/\PZ")
+fb1 = Fermion(out1, out_vtx).addArrow(0.2).addLabel(r"\APquark")
+fb2 = Fermion(out_vtx, out2).addArrow(0.8).addLabel(r"\Pquark")
+glu = Gluon(out_vtx.midpoint(out1), out_vtx.midpoint(out2))
+glu.invert().bend(0.5).addLabel("\Pgluon", displace=0.25)
+
+fmlwriter = FeynMLWriter("test")
+print fmlwriter.diagramToXML(fd)
+
