c9202205f97dd27d944a790684cdd43826a2c842
buckley <buckley@07e9fb06-9504-0410-9509-f90c9c1de517>
2007-05-13 18:52:05 +0000

    Updated docs etc.
    git-svn-id: http://svn.hepforge.org/pyfeyn@1905 07e9fb06-9504-0410-9509-f90c9c1de517

--- pyfeyn/trunk/setup.py
+++ pyfeyn/trunk/setup.py
@@ -1,19 +1,29 @@
 #! /usr/bin/env python
 
+"""PyFeyn: an easy-to-use Python library to help high-energy physicists draw Feynman diagrams."""
+
 ## Get setuptools
 import ez_setup
 ez_setup.use_setuptools()
 from setuptools import setup
 
+longdesc = """PyFeyn is a package which makes drawing Feynman diagrams simple and programmatic.
+Feynman diagrams are important constructs in perturbative field theory, so being able to draw them
+in a programmatic fashion is important if attempting to enumerate a large number of diagram
+configurations is important. The output quality of PyFeyn diagrams (into PDF or EPS formats)
+is very high, and special effects can be obtained by using constructs from PyX, which PyFeyn
+is based around."""
+
 ## Setup definition
 setup(name = 'pyfeyn',
-      version = '0.3.0',
+      version = '0.3.0b1',
       packages = ['pyfeyn'],
       install_requires = 'pyx >= 0.9',
       author = 'Andy Buckley',
       author_email = 'andy@insectnation.org',
       url = 'http://projects.hepforge.org/pyfeyn/',
       description = 'An easy-to-use Python library to help high-energy physicists draw Feynman diagrams.',
+      long_description = longdesc,
       keywords = 'feynman hep physics particle diagram',
       license = 'GPL',
       classifiers = ['Development Status :: 4 - Beta',
